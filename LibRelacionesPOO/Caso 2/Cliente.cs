a38082105c0cacb664bd5ba5b0b105fdd276b6bc
MSobrecasas <sns.mary@hotmail.com>
2023-01-13 13:27:27 -0300

    Agregar archivos de proyecto.

---
+++ LibRelacionesPOO/Caso 2/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using LibRelacionesPOO.Caso_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibRelacionesPOO.Caso_2
+{
+    public class Cliente
+    {
+        public List<Pedido> pedido;
+    }
+}
